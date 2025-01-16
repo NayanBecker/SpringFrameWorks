d382a27a859984929320d9b965a09ae85c4df97c
Nayan Becker <nayan.becker@expressosaomiguel.com.br>
2025-01-16 11:47:13 -0300

    Study notation Autowired, Service

---
+++ src/main/java/com/springinterface/demo/ioc_di/MeuComponent.java
@@ -0,0 +1,11 @@
+package com.springinterface.demo.ioc_di;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MeuComponent {
+    public String chamarMeuComponent(){
+        return "Chamando o método chamarMeuComponent()";
+    }
+    
+}
